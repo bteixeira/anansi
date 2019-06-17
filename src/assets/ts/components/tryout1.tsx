ee471a19f45a0b051207fbf4dce6bf79fd157ffa
Bruno Teixeira <bteixeira666@gmail.com>
2019-06-17 13:35:54 +0200

    Added dummy test page that only works depending on CORS. Removed Webstrap sample code.

---
+++ src/assets/ts/components/tryout1.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+
+interface Status {
+	url: string,
+	result: string,
+}
+
+export default class Tryout1 extends React.Component<{}, Status> {
+	constructor (props: {}) {
+		super(props)
+		this.state = {
+			url: 'http://example.com',
+			result: ' ',
+		}
+	}
+
+	render () {
+		return (
+				<div className="container">
+					<h1>Anansi</h1>
+					<div className="row">
+						<div className="col">
+							Starting URL
+							<input
+									type="url"
+									className="form-control"
+									onChange={this.handleUpdateUrl.bind(this)}
+									value={this.state.url}
+							/>
+							<button
+									type="button"
+									className="btn btn-primary"
+									onClick={this.handleClick.bind(this)}
+							>
+								Go
+							</button>
+						</div>
+						<div className="col">
+							Result
+							<pre className="border rounded p-2">
+								{this.state.result}
+							</pre>
+						</div>
+					</div>
+				</div>
+		)
+	}
+
+	handleClick () {
+		window.fetch(this.state.url, {mode: 'no-cors'}).then(response => {
+			response.text().then(text => {
+				this.setState({
+					result: text,
+				})
+			})
+		}).catch(reason => {
+			this.setState({
+				result: reason,
+			})
+		})
+	}
+
+	handleUpdateUrl (event: React.ChangeEvent<HTMLInputElement>) {
+		this.setState({
+			url: event.target.value,
+		})
+	}
+}
--- src/server/routers/defaultRouter.ts
+++ src/server/routers/defaultRouter.ts
@@ -1,43 +1,11 @@
 import {Request, Response, Router} from 'express'
-import Home from '../../assets/ts/components/home'
-import AuthorBooks from '../../assets/ts/components/authorBooks'
 import * as glue from '../glue'
-import Author, {AuthorInstance} from '../models/author'
-import apiRouter from './apiRouter'
-import Book from '../models/book'
+import Tryout1 from '../../assets/ts/components/tryout1'
 
 const defaultRouter = Router()
 
-console.log(Book)
-
 defaultRouter.get('/', (request: Request, response: Response) => {
-	response.locals.logger.info('Hello from the controller')
-	Author.findAll().then((authors: AuthorInstance[]) => {
-		glue.render(response, 'home', Home, {
-			authors: authors,
-		})
-	})
+	glue.render(response, 'tryout1', Tryout1, {})
 })
-
-defaultRouter.get('/authors/:authorId', (request: Request, response: Response) => {
-	Author.findById(
-		request.params.authorId,
-		{
-			include: [{
-				model: Book,
-			}],
-		}
-	).then((author: AuthorInstance) => {
-		glue.render(response, 'authorBooks', AuthorBooks, {
-			author: author,
-			books: author.Books,
-		})
-	}).catch(err => {
-		console.error(err) // TODO REPLACE WITH LOGGER
-		response.status(500).json(err)
-	})
-})
-
-defaultRouter.use('/api/', apiRouter)
 
 export default defaultRouter
