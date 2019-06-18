c9d7645da4c382155b33352522ac6acbb02a1de3
Bruno Teixeira <bteixeira666@gmail.com>
2019-06-18 15:36:59 +0200

    Added simple test with a working selector

--- src/assets/ts/components/tryout1.tsx
+++ src/assets/ts/components/tryout1.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 
 interface Status {
 	url: string,
-	result: string,
+	selector: string,
+	resultUrl: string,
+	resultSelector: string,
 }
 
 export default class Tryout1 extends React.Component<{}, Status> {
@@ -10,7 +12,9 @@ export default class Tryout1 extends React.Component<{}, Status> {
 		super(props)
 		this.state = {
 			url: 'http://example.com',
-			result: ' ',
+			selector: '',
+			resultUrl: ' ',
+			resultSelector: ' ',
 		}
 	}
 
@@ -24,22 +28,45 @@ export default class Tryout1 extends React.Component<{}, Status> {
 							<input
 									type="url"
 									className="form-control"
-									onChange={this.handleUpdateUrl.bind(this)}
+									onChange={this.handleChangeUrl.bind(this)}
 									value={this.state.url}
 							/>
-							<button
-									type="button"
-									className="btn btn-primary"
-									onClick={this.handleClick.bind(this)}
-							>
-								Go
-							</button>
 						</div>
 						<div className="col">
 							Result
 							<pre className="border rounded p-2">
-								{this.state.result}
+								{this.state.resultUrl}
 							</pre>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col">
+							Applied selector
+							<input
+								type="text"
+								className="form-control"
+								onChange={this.handleChangeSelector.bind(this)}
+								value={this.state.selector}
+							/>
+						</div>
+						<div className="col">
+							Result
+							<pre className="border rounded p-2">
+								{this.state.resultSelector}
+							</pre>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col">
+							<div className="btn-group">
+								<button
+										type="button"
+										className="btn btn-primary"
+										onClick={this.handleClick.bind(this)}
+								>
+									Go
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -47,23 +74,35 @@ export default class Tryout1 extends React.Component<{}, Status> {
 	}
 
 	handleClick () {
-		this.setState({result: '...'})
+		this.setState({resultUrl: '...'})
 		window.fetch(`api/fetch?url=${this.state.url}`).then(response => {
-			response.text().then(text => {
-				this.setState({
-					result: text,
-				})
+			return response.text()
+		}).then(text => {
+			this.setState({
+				resultUrl: text,
+			})
+			const parser = new DOMParser()
+			const doc = parser.parseFromString(text, 'text/html')
+			const len = $(doc).find(this.state.selector).length
+			this.setState({
+				resultSelector: `Found ${len} elements`,
 			})
 		}).catch(reason => {
 			this.setState({
-				result: reason,
+				resultUrl: reason,
 			})
 		})
 	}
 
-	handleUpdateUrl (event: React.ChangeEvent<HTMLInputElement>) {
+	handleChangeUrl (event: React.ChangeEvent<HTMLInputElement>) {
 		this.setState({
 			url: event.target.value,
+		})
+	}
+
+	handleChangeSelector (event: React.ChangeEvent<HTMLInputElement>) {
+		this.setState({
+			selector: event.target.value,
 		})
 	}
 }
